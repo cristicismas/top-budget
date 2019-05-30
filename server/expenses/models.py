802145f88f32f702fcb011538ee64d343399c4e6
Cristi Cismas <cristicismas99@gmail.com>
2019-05-30 17:16:46 +0300

    Fix bug in expense models

--- server/expenses/models.py
+++ server/expenses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import datetime
 
 class Expense(models.Model):
     value = models.IntegerField()
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField("Category")
     location = models.ForeignKey("Location", null=True, on_delete=models.SET_NULL)
     source = models.ForeignKey("Source", null=True, on_delete=models.SET_NULL)
-    date = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.datetime.now)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
