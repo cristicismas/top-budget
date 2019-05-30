a417055b0bb683407b7057311c4c55ce3c58d4c9
Cristi Cismas <cristicismas99@gmail.com>
2019-05-30 16:57:34 +0300

    Add models and make migrations

--- server/topbudget/expenses/models.py
+++ server/topbudget/expenses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    value = models.IntegerField()
+    category = models.ForeignKey("Category", null=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey("Location", null=True, on_delete=models.SET_NULL)
+    source = models.ForeignKey("Source", null=True, on_delete=models.SET_NULL)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=7)
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
