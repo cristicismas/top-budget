707b922c09b65979f6923dbbbc9d79242f591077
Cristi Cismas <cristicismas99@gmail.com>
2019-05-31 21:24:15 +0300

    Add features div in Hero.js

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -8,6 +8,19 @@ function Hero() {
         <h1>TopBudget</h1>
         <h2>Money management made easy.</h2>
       </div>
+      <ul id="features">
+        <div className="feature-group">
+          <li className="feature">Very easy to add expenses</li>
+          <li className="feature">Easy to understand graphs</li>
+          <li className="feature">Intuitive design</li>
+        </div>
+        
+        <div className="feature-group">
+          <li className="feature">Works great on any device</li>
+          <li className="feature">Ability to track how much you spend</li>
+          <li className="feature">Completely FREE!</li>
+        </div>
+      </ul>
     </section>
   );
 }
