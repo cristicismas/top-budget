be3010d695aaf4d22f6f34b3080a72c2463e1062
Cristi Cismas <cristicismas99@gmail.com>
2019-05-31 22:57:17 +0300

    Add form elements and style them

--- client/src/components/auth/AuthForm.js
+++ client/src/components/auth/AuthForm.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import '../../css/AuthForm.css';
 
 export class AuthForm extends Component {
   render() {
+    const formAction = this.props.type === 'login' ? 'Log In' : 'Sign Up';
+
     return (
       <div id="auth">
-        <h2>{this.props.type === "login" ? "Log In" : "Sign Up"}</h2>
+        <h2 id="form-title">{formAction}</h2>
+
+        <form id="auth-form" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              required
+              onChange={this.onChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Min. 5 characters"
+              minLength="5"
+              maxLength="70"
+              required
+              onChange={this.onChange} />
+          </div>
+
+          <button type="submit" id="submit-button">{formAction}</button>
+        </form>
       </div>
     )
   }
