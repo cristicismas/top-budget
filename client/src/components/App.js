f2cefa07ade2d1cebaa2d75aa3a1b84805b475cc
Cristi Cismas <cristicismas99@gmail.com>
2019-05-31 21:46:46 +0300

    Add react-router and make a Home component with its own route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import '../css/Header.css';
 
 import Header from './Header';
-import Hero from './Hero';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Hero from './Hero';
+
+function Home() {
+  return (
+    <div id="home">
+      <Hero />
+    </div>
+  )
+}
+
+export default Home
