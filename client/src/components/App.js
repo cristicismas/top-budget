e5119f658844a29617c7606089fd64c05dd82b95
Cristi Cismas <cristicismas99@gmail.com>
2019-05-31 21:08:58 +0300

    Add a hero component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../css/Header.css';
 
 import Header from './Header';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ client/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Hero.css';
+
+function Hero() {
+  return (
+    <section id="hero">
+      <div id="intro">
+        <h1>TopBudget</h1>
+        <h2>Money management made easy.</h2>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
