4a3e99b4a7343fb14af1ccea6fcc183dd2587b59
Cristi Cismas <cristicismas99@gmail.com>
2019-06-02 13:23:53 +0300

    Fix import bug in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -9,11 +15,6 @@ const store = createStore(reducer, composeEnhancer(
   applyMiddleware(thunk)
 ));
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 const app = (
   <Provider store={store}>
