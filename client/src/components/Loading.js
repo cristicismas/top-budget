97a437ef381d2acc48f3d78c44778a5406524b9a
Cristi Cismas <cristicismas99@gmail.com>
2019-06-08 12:55:42 +0300

    Change class to className in Loading.js

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -3,28 +3,28 @@ import "../css/Loading.css";
 
 function Loading() {
   return (
-    <div class="radar-spinner">
-      <div class="circle">
-        <div class="circle-inner-container">
-          <div class="circle-inner" />
+    <div className="radar-spinner">
+      <div className="circle">
+        <div className="circle-inner-container">
+          <div className="circle-inner" />
         </div>
       </div>
 
-      <div class="circle">
-        <div class="circle-inner-container">
-          <div class="circle-inner" />
+      <div className="circle">
+        <div className="circle-inner-container">
+          <div className="circle-inner" />
         </div>
       </div>
 
-      <div class="circle">
-        <div class="circle-inner-container">
-          <div class="circle-inner" />
+      <div className="circle">
+        <div className="circle-inner-container">
+          <div className="circle-inner" />
         </div>
       </div>
 
-      <div class="circle">
-        <div class="circle-inner-container">
-          <div class="circle-inner" />
+      <div className="circle">
+        <div className="circle-inner-container">
+          <div className="circle-inner" />
         </div>
       </div>
     </div>
