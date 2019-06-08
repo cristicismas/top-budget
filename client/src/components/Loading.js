0f409ab2d91ab9aa4bf9bec738ae46a629ac87e5
Cristi Cismas <cristicismas99@gmail.com>
2019-06-08 10:33:26 +0300

    Change Loading component

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import '../css/Loading.css';
+import React from "react";
+import "../css/Loading.css";
 
 function Loading() {
   return (
-    <div id="loading">
-      <div id="spinner" />
+    <div class="radar-spinner">
+      <div class="circle">
+        <div class="circle-inner-container">
+          <div class="circle-inner" />
+        </div>
+      </div>
+
+      <div class="circle">
+        <div class="circle-inner-container">
+          <div class="circle-inner" />
+        </div>
+      </div>
+
+      <div class="circle">
+        <div class="circle-inner-container">
+          <div class="circle-inner" />
+        </div>
+      </div>
+
+      <div class="circle">
+        <div class="circle-inner-container">
+          <div class="circle-inner" />
+        </div>
+      </div>
     </div>
   );
 }
