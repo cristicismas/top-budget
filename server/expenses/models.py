03b5e927e782ee6f46080a07d0cda033ee955a7d
Cristi Cismas <cristicismas99@gmail.com>
2019-06-12 21:45:33 +0300

    Change category's verbose_name_plural for django admin

--- server/expenses/models.py
+++ server/expenses/models.py
@@ -22,6 +22,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
