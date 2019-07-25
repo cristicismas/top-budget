2a964a7fd2917d21c2f5d28073ea3be1ad467601
Cristi Cismas <cristicismas99@gmail.com>
2019-07-25 13:25:02 +0300

    Add an arrow to the currency select group

--- client/src/components/dashboard/Settings/Currency.js
+++ client/src/components/dashboard/Settings/Currency.js
@@ -19,6 +19,10 @@ const Currency = props => {
         onChange={e => props.changeCurrency(e.target.value)}>
         {currencyOptions}
       </select>
+
+      <div className="currency-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#eee" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+      </div>
     </div>
   );
 };
