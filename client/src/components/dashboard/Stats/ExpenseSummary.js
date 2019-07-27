b9f105bc52aef3e1951768e24058b6e4bddd1a1b
Cristi Cismas <cristicismas99@gmail.com>
2019-07-27 16:33:17 +0300

    Add functionality to filter category values by date

--- client/src/components/dashboard/Stats/ExpenseSummary.js
+++ client/src/components/dashboard/Stats/ExpenseSummary.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import CURRENCY from '../../../constants/currencies';
+import FILTERS from '../../../constants/filters';
 
 const ExpenseSummary = props => {
   const { userdata } = props;
@@ -7,18 +8,36 @@ const ExpenseSummary = props => {
 
   const [showAllCategories, toggleShowAll] = useState(false);
 
+  const lastFilter = localStorage.getItem('lastFilter') ? localStorage.getItem('lastFilter') : FILTERS.WEEK;
+  const [filter, changeFilter] = useState(lastFilter);
+
+  const handleChangeFilter = filter => {
+    localStorage.setItem('lastFilter', filter);
+    changeFilter(filter);
+  };
+
   let currencySymbol = '';
   if (userdata) {
-    currencySymbol = CURRENCY[userdata.currency].symbol
-      ? CURRENCY[userdata.currency].symbol
-      : userdata.currency;
+    currencySymbol = CURRENCY[userdata.currency].symbol ? CURRENCY[userdata.currency].symbol : userdata.currency;
   }
 
   const calculateCategoryValue = category => {
     let value = 0;
+    const today = new Date();
 
     expenses.forEach(expense => {
-      if (expense.categories[0] === category.id) {
+      const expenseDate = new Date(expense.date);
+
+      const diffTime = Math.abs(today.getTime() - expenseDate.getTime());
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+      var daysToFilter = 0;
+
+      if (FILTERS[filter] === FILTERS.WEEK) daysToFilter = 7;
+      else if (FILTERS[filter] === FILTERS.MONTH) daysToFilter = 30;
+      else daysToFilter = 356;
+
+      if (diffDays < daysToFilter && expense.categories[0] === category.id) {
         value += Number(expense.value);
       }
     });
@@ -34,13 +53,22 @@ const ExpenseSummary = props => {
   const topCategoryValue = calculateCategoryValue(sortedCategories[0]);
 
   const calculateBarWidth = categoryValue => {
-    const MAX_WIDTH = 150;
+    const MAX_WIDTH = 250;
 
     const fillPercentage = (categoryValue * 100) / topCategoryValue;
     const width = MAX_WIDTH / (100 / fillPercentage);
 
     return width;
   };
+
+  const filterButtons = Object.keys(FILTERS).map(filterName => (
+    <button
+      key={'filter-' + filterName}
+      onClick={() => handleChangeFilter(filterName)}
+      className={`filter-btn ${filter === filterName ? 'active' : ''}`}>
+      {FILTERS[filterName]}
+    </button>
+  ));
 
   const categoryGroups = sortedCategories.map(category => {
     const categoryValue = calculateCategoryValue(category);
@@ -66,15 +94,9 @@ const ExpenseSummary = props => {
 
   return (
     <section id="expense-summary">
-      <ul className="filters">
-        <button className="filter-btn">Week</button>
-        <button className="filter-btn">Month</button>
-        <button className="filter-btn">Year</button>
-      </ul>
+      <ul className="filters">{filterButtons}</ul>
 
-      <ul className="summary">
-        {showAllCategories ? categoryGroups : categoryGroups.splice(0, 4)}
-      </ul>
+      <ul className="summary">{showAllCategories ? categoryGroups : categoryGroups.splice(0, 4)}</ul>
 
       <button onClick={() => toggleShowAll(!showAllCategories)} id="show-all">
         Show {showAllCategories ? 'less' : 'all'}
---
+++ client/src/constants/filters.js
@@ -0,0 +1,5 @@
+export default {
+  WEEK: 'Week',
+  MONTH: 'Month',
+  YEAR: 'Year'
+};
