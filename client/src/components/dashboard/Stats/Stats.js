1f028f1ed9349c5c099e9f05b66db0e7bad6597b
Cristi Cismas <cristicismas99@gmail.com>
2019-08-23 21:23:40 +0300

    Handle empty state for Stats.js

--- client/src/components/dashboard/Stats/Stats.js
+++ client/src/components/dashboard/Stats/Stats.js
@@ -4,20 +4,25 @@ import '../../../css/Stats.css';
 import ExpenseSummary from './ExpenseSummary';
 import BudgetWheel from './BudgetWheel';
 import RecentExpenses from './RecentExpenses';
+import Message from '../../Message';
 
 const Stats = props => {
   const { expenses, user } = props;
 
-  return (
-    <section id="stats">
-      <div className="flex-group">
-        <ExpenseSummary expenses={expenses} userdata={user.userdata} />
-        <BudgetWheel expenses={expenses} userdata={user.userdata} />
-      </div>
+  if (expenses.expenses.length) {
+    return (
+      <section id="stats">
+        <div className="flex-group">
+          <ExpenseSummary expenses={expenses} userdata={user.userdata} />
+          <BudgetWheel expenses={expenses} userdata={user.userdata} />
+        </div>
 
-      <RecentExpenses />
-    </section>
-  );
-}
+        <RecentExpenses />
+      </section>
+    );
+  } else {
+    return <Message message={'Please add at least one expense before viewing your stats.'} />;
+  }
+};
 
 export default Stats;
