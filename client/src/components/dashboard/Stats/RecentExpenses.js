0ab83d103832f410040ccab3f5eed8cf4af6eeaf
Cristi Cismas <cristicismas99@gmail.com>
2019-09-14 13:58:06 +0300

    Refactor RecentExpenses and make sure to always add new days at the top

--- client/src/components/dashboard/Stats/RecentExpenses.js
+++ client/src/components/dashboard/Stats/RecentExpenses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import moment from 'moment';
+import { getRecentDays, formatDate } from '../../../utils/recent';
 import '../../../css/RecentExpenses.css';
 
 import DetailedExpense from './DetailedExpense';
@@ -7,55 +7,15 @@ import DetailedExpense from './DetailedExpense';
 const RecentExpenses = props => {
   const { expenses, categories, sources, locations } = props.expenses;
 
-  const expenseDays = [];
+  const recentDays = getRecentDays(expenses);
 
-  expenses.forEach(expense => {
-    const expenseDay = moment(expense.date).calendar(null, {
-      sameDay: '[Today]',
-      lastDay: '[Yesterday]',
-      lastWeek: '[Last] dddd',
-      sameElse: 'DD/MM'
-    });
+  const days = recentDays.map(currentDay => {
+    const formattedDate = formatDate(currentDay.day);
 
-    // Find out if day is already in array and store it in a variable.
-    let dayFromArrayIndex = null;
-    const dayFromArray = expenseDays.find((arrayDay, index) => {
-      if (arrayDay.day === expenseDay) {
-        dayFromArrayIndex = index;
-        return true;
-      }
-      return false;
-    });
-
-    if (dayFromArray) {
-      // If an expense has been added since last render, compare it to the last expense and
-      // add it in the right place in the array.
-      const isExpenseNew = moment(expense.date).isAfter(dayFromArray.expenses[0].date);
-
-      let updatedExpenses = [...dayFromArray.expenses, expense];
-      if (isExpenseNew) {
-        updatedExpenses = [expense, ...dayFromArray.expenses];
-      }
-
-      expenseDays[dayFromArrayIndex] = {
-        day: expenseDay,
-        dayValue: dayFromArray.dayValue + Number(expense.value),
-        expenses: updatedExpenses
-      };
-    } else {
-      expenseDays.push({
-        day: expenseDay,
-        dayValue: Number(expense.value),
-        expenses: [expense]
-      });
-    }
-  });
-
-  const days = expenseDays.map(currentDay => {
     return (
-      <div className="expense-day" key={currentDay.day}>
+      <div className="expense-day" key={formattedDate}>
         <div className="day-info flex-group">
-          <h3 className="expense-day">{currentDay.day}</h3>
+          <h3 className="expense-day">{formattedDate}</h3>
           <h4 className="day-price">{currentDay.value}</h4>
         </div>
 
---
+++ client/src/utils/recent.js
@@ -0,0 +1,66 @@
+import moment from 'moment';
+
+export const getRecentDays = expenses => {
+  let recentDays = [];
+
+  expenses.forEach(expense => {
+    const expenseDay = moment(expense.date);
+
+    // Find out if day is already in array and store it in a variable.
+    let dayFromArrayIndex = null;
+    const dayFromArray = recentDays.find((arrayDay, index) => {
+      if (formatDate(arrayDay.day) === formatDate(expenseDay)) {
+        dayFromArrayIndex = index;
+        return true;
+      }
+      return false;
+    });
+
+    if (dayFromArray) {
+      // If an expense has been added since last render, compare it to the last expense and
+      // add it in the right place in the array.
+      const isExpenseNew = moment(expense.date).isAfter(dayFromArray.expenses[0].date);
+
+      let updatedExpenses = [...dayFromArray.expenses, expense];
+      if (isExpenseNew) {
+        updatedExpenses = [expense, ...dayFromArray.expenses];
+      }
+
+      recentDays[dayFromArrayIndex] = {
+        day: expenseDay,
+        dayValue: dayFromArray.dayValue + Number(expense.value),
+        expenses: updatedExpenses
+      };
+    } else {
+      const isDayNew = recentDays[0] ? moment(expense.date).isAfter(recentDays[0].day) : true;
+
+      if (isDayNew) {
+        recentDays = [
+          {
+            day: expenseDay,
+            dayValue: Number(expense.value),
+            expenses: [expense]
+          },
+          ...recentDays
+        ];
+      } else {
+        recentDays.push({
+          day: expenseDay,
+          dayValue: Number(expense.value),
+          expenses: [expense]
+        });
+      }
+    }
+  });
+
+  return recentDays;
+};
+
+export const formatDate = date => {
+  return date.calendar(null, {
+    sameDay: '[Today]',
+    lastDay: '[Yesterday]',
+    lastWeek: '[Last] dddd',
+    sameElse: 'DD/MM'
+  });
+}
