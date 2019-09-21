7f8bc8f02ee9291ae713318a6ec201b7a56ee246
Cristi Cismas <cristicismas99@gmail.com>
2019-09-21 10:11:50 +0300

    Make the Message fadeout on a timeout

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,49 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TYPES from '../constants/messageTypes';
 import '../css/Message.css';
 
 const Message = props => {
-  let style = {
-    color: '#333',
-    textAlign: 'center',
-    padding: '5px 10px',
-    margin: '20px auto',
-    opacity: '.9',
-    width: '250px',
-    position: 'relative',
-    borderRadius: '5px'
-  };
+  const { message, setMessage } = props;
+
+  const [opacity, changeOpacity] = useState(0);
+
+  // Fadeout the component and clear the message.
+  useEffect(() => {
+    changeOpacity(1);
+
+    let messageTimeout, opacityTimeout = null;
+
+    opacityTimeout = setTimeout(() => {
+      changeOpacity(0);
+
+      messageTimeout = setTimeout(() => {
+        setMessage('', '');
+      }, 500);
+    }, 2500);
+
+    return () => {
+      clearTimeout(opacityTimeout);
+      clearTimeout(messageTimeout);
+    }
+  }, [message, setMessage]);
+
+
+  let style = {};
 
   if (props.type === TYPES.ERROR) {
     style = {
-      ...style,
+      opacity,
       color: '#ff2525',
       border: '1px solid #900404'
     };
   } else if (props.type === TYPES.SUCCESS) {
     style = {
-      ...style,
+      opacity,
       color: '#27e24b',
       border: '1px solid #17630a'
     };
   } else {
     style = {
-      ...style,
+      opacity,
       color: '#f1e979',
       border: '1px solid #e1b400'
     };
   }
 
-  if (!props.message) return null;
-
   return (
     <div className="message" style={style}>
-      <span
-        onClick={props.clearMessage}
-        className="close-message-btn">
-        ✕
-      </span>
-
       <p>{props.message}</p>
     </div>
   );
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,29 +8,13 @@ import Stats from './Stats/Stats';
 import AddExpense from './AddExpense/AddExpense';
 import Message from '../Message';
 
-import {
-  getExpenses,
-  deleteExpense,
-  addExpense
-} from '../../store/actions/expenses';
+import { getExpenses, deleteExpense, addExpense } from '../../store/actions/expenses';
 
-import {
-  getCategories,
-  deleteCategory,
-  addCategory
-} from '../../store/actions/categories';
+import { getCategories, deleteCategory, addCategory } from '../../store/actions/categories';
 
-import {
-  getLocations,
-  deleteLocation,
-  addLocation
-} from '../../store/actions/locations';
+import { getLocations, deleteLocation, addLocation } from '../../store/actions/locations';
 
-import {
-  getSources,
-  deleteSource,
-  addSource
-} from '../../store/actions/sources';
+import { getSources, deleteSource, addSource } from '../../store/actions/sources';
 
 import { loadUser, updateUserSettings } from '../../store/actions/user';
 
@@ -56,6 +40,7 @@ class Dashboard extends Component {
   }
 
   setMessage(message, type) {
+    console.log(message);
     this.setState({
       message,
       type
@@ -72,51 +57,32 @@ class Dashboard extends Component {
       return (
         <section id="dashboard">
           <nav>
-            <Link
-              to="/dashboard/settings"
-              className={pathname === '/dashboard/settings' ? 'active' : null}>
+            <Link to="/dashboard/settings" className={pathname === '/dashboard/settings' ? 'active' : null}>
               Settings
             </Link>
 
-            <Link
-              to="/dashboard"
-              className={pathname === '/dashboard' ? 'active' : null}>
+            <Link to="/dashboard" className={pathname === '/dashboard' ? 'active' : null}>
               Add Expense
             </Link>
 
-            <Link
-              to="/dashboard/stats"
-              className={pathname === '/dashboard/stats' ? 'active' : null}>
+            <Link to="/dashboard/stats" className={pathname === '/dashboard/stats' ? 'active' : null}>
               Stats
             </Link>
           </nav>
 
-          <Message
-            message={message}
-            type={type}
-            clearMessage={() => this.setMessage('', '')}
-          />
+          {message && (
+            <Message message={message} type={type} setMessage={(message, type) => this.setMessage(message, type)} />
+          )}
 
           <Route
             exact
             path="/dashboard"
-            render={() => (
-              <AddExpense
-                {...this.props}
-                setMessage={(message, type) => this.setMessage(message, type)}
-              />
-            )}
+            render={() => <AddExpense {...this.props} setMessage={(message, type) => this.setMessage(message, type)} />}
           />
 
-          <Route
-            path="/dashboard/settings"
-            render={() => <Settings {...this.props} />}
-          />
+          <Route path="/dashboard/settings" render={() => <Settings {...this.props} />} />
 
-          <Route
-            path="/dashboard/stats"
-            render={() => <Stats {...this.props} />}
-          />
+          <Route path="/dashboard/stats" render={() => <Stats {...this.props} />} />
         </section>
       );
     } else {
