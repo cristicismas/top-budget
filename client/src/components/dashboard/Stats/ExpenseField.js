d30635974a855cb6cb5d67e28854827d62492d0f
Cristi Cismas <cristicismas99@gmail.com>
2019-09-22 20:53:44 +0300

    Change RecentExpenses' 'Not specified' text to a long dash, and decrease the expense width

--- client/src/components/dashboard/Stats/ExpenseField.js
+++ client/src/components/dashboard/Stats/ExpenseField.js
@@ -5,7 +5,7 @@ import ColorCircle from './ColorCircle';
 const ExpenseField = props => (
   <div className="expense-field flex-group">
     <ColorCircle color={props.field ? props.field.color : '#888'} />
-    {props.field ? props.field.name : 'Not specified'}
+    {props.field ? props.field.name : '―'}
   </div>
 );
 
