b5aca5557573b6f03ffb4416f91e855360a133de
Cristi Cismas <cristicismas99@gmail.com>
2019-09-25 16:23:50 +0300

    Refactor Chart.js by extracting functions in the utils/chart.js

--- client/src/components/dashboard/Stats/Chart.js
+++ client/src/components/dashboard/Stats/Chart.js
@@ -2,75 +2,9 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import '../../../css/Chart.css';
 
-import { convertHexToRgb, getCategoryValues, getLastSevenDays } from '../../../utils/chart';
+import { getDatasets, getChartOptions, getLastSevenDays } from '../../../utils/chart';
 
-const chartOptions = {
-  maintainAspectRatio: false,
-  scales: {
-    xAxes: [
-      {
-        stacked: true,
-        barPercentage: 0.8,
-        gridLines: {
-          color: 'rgba(100, 100, 100, .2)'
-        }
-      }
-    ],
-    yAxes: [
-      {
-        stacked: true,
-        gridLines: {
-          color: 'rgba(100, 100, 100, .2)'
-        }
-      }
-    ]
-  }
-};
-
-const getDatasets = (categories, expenses) => {
-  let datasets = [];
-  const data = {};
-
-  categories.forEach(category => {
-    const categoryValues = getCategoryValues(category, expenses);
-    data[category.name] = categoryValues;
-  });
-
-  categories.forEach(category => {
-    const rgb = convertHexToRgb(category.color);
-
-    const categoryColor = `rgba(${rgb}, .5)`;
-    const hoverCategoryColor = `rgba(${rgb}, .7)`;
-
-    datasets.push({
-      label: category.name,
-      backgroundColor: categoryColor,
-      borderColor: categoryColor,
-      borderWidth: 3,
-      hoverBackgroundColor: hoverCategoryColor,
-      hoverBorderColor: hoverCategoryColor,
-      data: data[category.name]
-    });
-  });
-
-  // Push 'not defined' category.
-  const noCategoryDefinedValues = getCategoryValues(null, expenses);
-
-  const categoryColor = 'rgba(136, 136, 136, .5)';
-  const hoverCategoryColor = 'rgba(136, 136, 136, .7)';
-
-  datasets.push({
-    label: 'Not specified',
-    backgroundColor: categoryColor,
-    borderColor: categoryColor,
-    borderWidth: 3,
-    hoverBackgroundColor: hoverCategoryColor,
-    hoverBorderColor: hoverCategoryColor,
-    data: noCategoryDefinedValues
-  });
-
-  return datasets;
-};
+const chartOptions = getChartOptions();
 
 const Chart = props => {
   const { categories, expenses } = props.expenses;
--- client/src/utils/chart.js
+++ client/src/utils/chart.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-export const convertHexToRgb = hex => {
+const convertHexToRgb = hex => {
   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
   const r = parseInt(result[1], 16);
@@ -18,7 +18,7 @@ const belongsToDate = (expense, date) => {
   }
 };
 
-export const getCategoryValues = (category, expenses) => {
+const getCategoryValues = (category, expenses) => {
   let values = [];
   const days = getLastSevenDays();
 
@@ -49,6 +49,76 @@ export const getCategoryValues = (category, expenses) => {
   }
 
   return values;
+};
+
+export const getDatasets = (categories, expenses) => {
+  let datasets = [];
+  const data = {};
+
+  categories.forEach(category => {
+    const categoryValues = getCategoryValues(category, expenses);
+    data[category.name] = categoryValues;
+  });
+
+  categories.forEach(category => {
+    const rgb = convertHexToRgb(category.color);
+
+    const categoryColor = `rgba(${rgb}, .5)`;
+    const hoverCategoryColor = `rgba(${rgb}, .7)`;
+
+    datasets.push({
+      label: category.name,
+      backgroundColor: categoryColor,
+      borderColor: categoryColor,
+      borderWidth: 3,
+      hoverBackgroundColor: hoverCategoryColor,
+      hoverBorderColor: hoverCategoryColor,
+      data: data[category.name]
+    });
+  });
+
+  // Push 'not defined' category.
+  const noCategoryDefinedValues = getCategoryValues(null, expenses);
+
+  const categoryColor = 'rgba(136, 136, 136, .5)';
+  const hoverCategoryColor = 'rgba(136, 136, 136, .7)';
+
+  datasets.push({
+    label: 'Not specified',
+    backgroundColor: categoryColor,
+    borderColor: categoryColor,
+    borderWidth: 3,
+    hoverBackgroundColor: hoverCategoryColor,
+    hoverBorderColor: hoverCategoryColor,
+    data: noCategoryDefinedValues
+  });
+
+  return datasets;
+};
+
+export const getChartOptions = () => {
+  return {
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [
+        {
+          stacked: true,
+          barPercentage: 0.8,
+          gridLines: {
+            color: 'rgba(100, 100, 100, .2)'
+          }
+        }
+      ],
+      yAxes: [
+        {
+          stacked: true,
+          gridLines: {
+            color: 'rgba(100, 100, 100, .2)'
+          }
+        }
+      ]
+    }
+  };
 };
 
 export const getLastSevenDays = () => {
