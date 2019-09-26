20f076cc8332c623333e38c322990258cee76fec
Cristi Cismas <cristicismas99@gmail.com>
2019-09-26 17:00:33 +0300

    Close overlay on outside click

--- client/src/components/dashboard/Overlay.js
+++ client/src/components/dashboard/Overlay.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import '../../css/Overlay.css';
 
+const useOutsideClickDetector = (ref, closeOverlay) => {
+  const handleOutsideClick = event => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      closeOverlay();
+    }
+  }
+
+  useEffect(() => {
+    // Bind the event listener
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+}
+
 const Overlay = props => {
+  const overlayRef = useRef(null);
+  useOutsideClickDetector(overlayRef, props.closeOverlay);
+
   return (
     <div className="overlay-container">
-      <div className="overlay">
+      <div className="overlay" ref={overlayRef}>
         <button type="button" className="close-overlay-btn" onClick={() => props.closeOverlay()}>
           ✕
         </button>
