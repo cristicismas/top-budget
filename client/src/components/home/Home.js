af0e0692b93db18b2687a36a150c1966c82c8175
Cristi Cismas <cristicismas99@gmail.com>
2019-10-13 15:46:30 +0300

    Add a scroll up button on the homepage

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import ICONS from '../../constants/icons';
+import '../../css/Home.css';
+
 import Hero from './Hero';
 import Features from './Features';
+import Icon from '../Icon';
+
+const isElementInViewport = (element, offset) => {
+  var rect = element.getBoundingClientRect();
+
+  return (
+    rect.bottom >= 0 &&
+    rect.right >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) - offset &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const scrollUp = () => {
+  const header = document.querySelector('header');
+
+  header.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+};
 
 const Home = () => {
+  useEffect(() => {
+    const header = document.querySelector('header');
+    const scrollUpButton = document.getElementById('scroll-up-btn');
+
+    window.onscroll = e => {
+      if (header && scrollUpButton) {
+        if (isElementInViewport(header, 0)) {
+          scrollUpButton.style.opacity = 0;
+          scrollUpButton.style.cursor = 'default';
+        } else {
+          scrollUpButton.style.opacity = 1;
+          scrollUpButton.style.cursor = 'pointer';
+        }
+      } else {
+        console.log(header, scrollUpButton);
+      }
+    };
+  }, []);
+
   return (
     <div id="home">
       <Hero />
       <Features />
-    </div>
-  )
-}
 
-export default Home
+      <button type="button" id="scroll-up-btn" onClick={scrollUp}>
+        <Icon icon={ICONS.ARROW} size={35} fill="#ccc" />
+      </button>
+    </div>
+  );
+};
+
+export default Home;
