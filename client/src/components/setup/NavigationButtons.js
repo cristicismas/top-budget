de71466c625fed2bd3a82ab0b63c2c3fb9ab87b2
Cristi Cismas <cristicismas99@gmail.com>
2019-10-20 21:28:02 +0300

    Add icons to the setup navigation buttons

--- client/src/components/setup/NavigationButtons.js
+++ client/src/components/setup/NavigationButtons.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addMessage } from '../../store/actions/messages';
+import ICONS from '../../constants/icons';
 import './NavigationButtons.css';
+
+import Icon from '../general/Icon';
 
 const NavigationButtons = props => {
   const { expenses, showBack, showNext } = props;
@@ -19,14 +22,19 @@ const NavigationButtons = props => {
   return (
     <nav id="nav-buttons">
       <button className={showBack ? '' : 'disabled'} onClick={showBack ? props.onBack : () => {}}>
+        <Icon icon={ICONS.ARROW} size={20} fill="#eee" className="back-arrow icon" />
         Back
       </button>
 
       {showNext ? (
-        <button onClick={props.onNext}>Next</button>
+        <button onClick={props.onNext}>
+          Next
+          <Icon icon={ICONS.ARROW} size={20} fill="#eee" className="next-arrow icon" />
+        </button>
       ) : (
         <button className={expenses.length ? '' : 'disabled'} onClick={handleDashboardClick}>
           Done
+          <Icon icon={ICONS.CHECK} size={20} fill="#eee" className="check-icon icon" />
         </button>
       )}
     </nav>
